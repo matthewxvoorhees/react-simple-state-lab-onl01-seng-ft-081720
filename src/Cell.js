7f066f642b1ec3cde9997a7fe84daf4ce2563a9b
matthewxvoorhees <matthew.crnich@gmail.com>
2021-01-03 14:01:20 -0700

    commit tree 0b08c9554221c66171d3569951e4cea4cb43d095

---
+++ src/Cell.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+export default class Cell extends React.Component {
+    constructor(props) {
+        super()
+        this.state = {
+            color: props.value
+        }
+
+    }
+    colorChange = () => {
+       this.setState({
+           color: '#333'
+       }) 
+    }
+
+    render() {
+        return(
+        <div className="cell" style={{backgroundColor: this.state.color}} onClick={this.colorChange}>
+        </div>
+        )
+    }
+}
